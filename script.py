447ed4c059927b2ae6a0e2cb96ae53c26c233d7b
Ezequias Antunes <ezequiasantunes18@gmail.com>
2023-11-20 14:57:12 -0300

    feat: reduz resoluação da câmera e salva as imagens

--- script.py
+++ script.py
@@ -2,7 +2,8 @@ import cv2
 import MobileNetModule as mnModule
 import RequestApp
 import argparse
-
+import random
+import os
 
 parser = argparse.ArgumentParser(
                     prog='Detector de objetos',
@@ -13,17 +14,29 @@ parser.add_argument('-s', '--send-to-app',
                     dest='send_to_app',
                     action='store_true')
 
+parser.add_argument('-r', '--render-frame',
+                    dest='render_frame',
+                    action='store_true')
+
 args = parser.parse_args()
 
 cap = cv2.VideoCapture(0)
 width =  cap.get(3)
 height = cap.get(4)
 
-cap.set(3, 640)
-cap.set(4, 480)
+# cap.set(3, 640)
+# cap.set(4, 480)
+
+cap.set(3, 320)
+cap.set(4, 240)
 
 myModel = mnModule.msSSD("ssd-mobilenet-v2", 0.5)
 request = RequestApp.RequestApp()
+
+image_path=r"/home/estudante/Documentos/Estudo Jetson Nano/jetson-inference/build/aarch64/bin/backup"
+os.chdir(image_path)
+
+frame_identifier = 0
 
 while True:
     _, img = cap.read()
@@ -32,5 +45,11 @@ while True:
     if args.send_to_app is True:
         request.send(img)
 
-    cv2.imshow("Image", img)
+    if args.render_frame is True:
+        cv2.imshow("Image", img)
+
+    frame_identifier+=1
+    if (frame_identifier % 15) == 0:
+        cv2.imwrite(f"image{random.randint(1, 20)}.png", img)
+
     cv2.waitKey(1)
