25a4a3d57b292d06459f48fd3c2776326f11b609
Ezequias Antunes <ezequiasantunes18@gmail.com>
2023-11-20 14:59:49 -0300

    feat: cria agente pra iniciar o script de detecção de objetos

---
+++ agents/OD_Agent.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+process_name="$OD_Module"
+directory_OD_Program="$PATH_OD_Module"
+
+init_OD_Module() {
+    # Move para o diretório específico
+    cd "$directory_OD_Program" || exit
+
+    # Python command
+    comando_python="python3 $process_name -s"
+
+    # Executing python command
+    $comando_python
+    echo "Script executing..."
+}
+
+echo "Started OD Agent"
+
+# Check camera conectivity
+if [ -n "$(ls /dev/video* 2>/dev/null)" ]; then
+    echo "Camera detected."
+    init_OD_Module
+else
+    echo "Camera not detected"
+    exit 1
+fi
